0a18b0f510bb26c2c5e32a3113088d2aa187d260
suchart <suchartsuttinak@gmail.com>
2022-09-06 16:16:51 +0700

    frontend

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const authService = axios.create({
   baseURL: "http://localhost:8000/api/",
+  withCredentials: true,
   headers: {
     "Content-type": "application/json",
     Accept: "application/json",
